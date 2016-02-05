3974e2f7e60ab60b50d522d2956d3aefc274410f
meshnesh <tonnie.nesh@gmail.com>
2016-02-05 11:12:19 +0300

    a code that reverses the string

---
+++ reverse.java
@@ -0,0 +1,12 @@
+package toys;
+
+public class reverse {
+
+	public static void main(String a[]) {
+		String s = "Moringa school";
+		String r = new StringBuffer(s).reverse().toString();
+		System.out.println(r);
+		}
+
+	}
+
