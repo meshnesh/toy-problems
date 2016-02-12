e1ddd631ca534efdc00a66c691a43947956d8883
meshnesh <tonnie.nesh@gmail.com>
2016-02-12 19:53:28 +0300

    added remove duplicate code

---
+++ removeDuplicate.java
@@ -0,0 +1,24 @@
+package toys;
+
+import java.util.HashMap;
+
+public class removeDuplicate{
+
+	public static void main(String[] args) {
+		String full = "antony";//sample string
+	    String remove=" ";//string with no duplicates
+	    HashMap <Integer,Character> charStore = new HashMap<Integer,Character>();//create a hashmap to store the char's
+	    char [] charArray = full.toCharArray();
+	    for (Character c : charArray)//for each char
+	    	{
+	            if (!charStore.containsValue(c))//if the char is not already in the hashmap
+	                {
+	                    remove=remove+c.toString();//add the char to the output string
+	                    charStore.put(c.hashCode(),c);//and add the char to the hashmap
+	                }
+	        }
+
+	        System.out.println(remove);//final string
+	    }
+		
+	}
