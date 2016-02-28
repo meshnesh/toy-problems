698133f718ed33148da0db4b51348d8e21f4dbd8
meshnesh <tonnie.nesh@gmail.com>
2016-02-28 19:35:10 +0300

    linkedlistdemo.java

---
+++ LinkedListDemo.java
@@ -0,0 +1,19 @@
+
+public class LinkedListDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList linkedList = new LinkedList();
+		linkedList.insertAtHead(5);
+		linkedList.insertAtHead(10);
+		linkedList.insertAtHead(12);
+		linkedList.insertAtHead(19);
+		linkedList.insertAtHead(20);
+		
+		System.out.println(linkedList);
+	}
+
+}
