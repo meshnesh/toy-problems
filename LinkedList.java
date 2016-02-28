c2775d99dd02959d43ba51737d5d9e5059bebcac
meshnesh <tonnie.nesh@gmail.com>
2016-02-28 19:34:21 +0300

    added linked.java

---
+++ LinkedList.java
@@ -0,0 +1,23 @@
+
+public class LinkedList {
+	private Node head;
+	
+	public void insertAtHead(int data){
+		Node newNode = new Node(data);
+		newNode.setNextNode(this.head);
+		this.head = newNode;
+	}
+	@Override
+	public String toString(){
+		String result = "{";
+		Node current = this.head;
+		
+		while(current != null){
+			result += current.toString() + ", ";
+			current = current.getNextNode();
+		}
+		
+		result += "}";
+		return result;
+	}
+}
