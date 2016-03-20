a7e644e9b5ff6510c74035b55f808727493ae008
meshnesh <tonnie.nesh@gmail.com>
2016-03-20 23:52:38 +0300

    created a greetings program

---
+++ greetings.py
@@ -0,0 +1,14 @@
+def main():
+
+    
+    print('hello world')
+    print("What is your name?")
+
+    myName = input()
+
+    print('It is good to meet you, ' + myName)
+    print('The length of your name is:')
+    print(len(myName))
+
+
+if __name__ == '__main__': main()
