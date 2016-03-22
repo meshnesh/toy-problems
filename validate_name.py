d824a24b4c80ee3a7ad7b301e6eae6469c75350b
meshnesh <tonnie.nesh@gmail.com>
2016-03-22 04:39:50 +0300

    validate_name.py validates the persons name before approving access

---
+++ validate_name.py
@@ -0,0 +1,27 @@
+
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+
+print('Thank you!')
+
+
+
+
+
+
+
+
+
+# while True:
+#        print('Who are you?')
+#        name = input()
+#        if name != 'Joe':
+#            continue
+#        print('Hello, Joe. What is the password? (It is a fish.)')
+#        password = input()
+#        if password == 'swordfish':
+#            break
+# print('Access granted.')
