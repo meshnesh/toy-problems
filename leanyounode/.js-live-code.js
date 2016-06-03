401d5256d53cc4a5967f585355dac7e42cd71594
meshnesh <tonny.nesh@gmail.com>
2016-06-03 10:43:29 +0300

    babysteps.js

---
+++ leanyounode/.js-live-code.js
@@ -0,0 +1,10 @@
+function Sum(a, b) {
+
+      
+    var sum = a + b  
+    return sum
+
+}
+
+
+console.log(console)
--- leanyounode/program.js
+++ leanyounode/program.js
@@ -1,2 +1,6 @@
-var node = "HELLO WORLD";
-console.log(node);
+var steps = 0
+
+    for (var i = 2; i < process.argv.length; i++)
+      steps += Number(process.argv[i])
+
+    console.log(steps)
