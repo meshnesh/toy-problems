7052c21dae2271fffdb8080b5286e0f47710f94d
meshnesh <tonny.nesh@gmail.com>
2016-06-13 03:11:14 +0300

    started list filter code in node

--- leanyounode/program.js
+++ leanyounode/program.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = process.argv[2];
 
-fs.readFile(path, 'utf8', function(err,data) {
-  var lines = data.split('\n');
+fs.readdir(path, 'utf8', function(err,list) {
+  var lines = list.split('\n');
   console.log(lines.length-1);
 });
