c0ef141c7a8ff3bccce7916f9e1603da0c71b29a
meshnesh <tonny.nesh@gmail.com>
2016-06-18 14:15:55 +0300

    added srver with express

--- nodejs/web-server/server.js
+++ nodejs/web-server/server.js
@@ -5,10 +5,23 @@ var port = config.port;
 var express = require("express");
 
 var app = express();
+
+
+app.use(app.route);
+app.use(express.static(__dirname + "/public"));
+
+
 app.get("/", function (request, response) {
     response.send("Am doing it!");
 });
 
-var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
-});
+// TO RUN THE SERVER
+
+var server = app.listen(port, host);
+
+// IF YOU WANT TO GET NOTIFIED THE SERVER IS RUNNING
+
+
+// var server = app.listen(port, function() {
+//     console.log('Listening on port %d', server.address().port);
+// });
